c78d28d0c6f461f91218b6889030e76b7c900341
Atualizado <matsui@atualizado.com.br>
2018-04-24 12:40:38 -0300

    submissão de arquivos
    Arquivos das aulas de autenticação e real time database do Curso Firebase

---
+++ curso-fire/js/app.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyB8NyuRhgDFX4LtTFiEgRXYcTpbqyG7YCw",
+    authDomain: "my-project-1500752761601.firebaseapp.com",
+    databaseURL: "https://my-project-1500752761601.firebaseio.com",
+    projectId: "my-project-1500752761601",
+    storageBucket: "my-project-1500752761601.appspot.com",
+    messagingSenderId: "183268834321"
+};
+firebase.initializeApp(config);
