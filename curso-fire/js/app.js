8b16012b3f67de7fa7e1f3589fd769c7a512ccab
Matsui <contato@atualizado.com.br>
2018-05-18 15:45:32 -0300

    Utilizando armazenamento em nuvem Storage

--- curso-fire/js/app.js
+++
@@ -1,9 +0,0 @@
-var config = {
-    apiKey: "ApiKeyDaBagaçaToda",
-    authDomain: "AutenticationURL.firebaseapp.com",
-    databaseURL: "https://projetoURL.firebaseio.com",
-    projectId: "PROJETOID",
-    storageBucket: "my-project-storageNumber.appspot.com",
-    messagingSenderId: "8394792374IdSender"
-};
-firebase.initializeApp(config);
