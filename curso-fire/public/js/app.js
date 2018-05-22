a20ede741570bf17bd38a12169a7fc0d7dc61c6c
Matsui <contato@atualizado.com.br>
2018-05-22 13:09:33 -0300

    Organizando os arquivos
    Reorganizando a estrutura de arquivos para subir pro Hosting Google

---
+++ curso-fire/public/js/app.js
@@ -0,0 +1,10 @@
+var config = {
+    apiKey: "AIzaSyCYvo2WUxpBCdP0y26L6JesG4VgE-zr2es",
+    authDomain: "curso-firebase-basico.firebaseapp.com",
+    databaseURL: "https://curso-firebase-basico.firebaseio.com",
+    projectId: "curso-firebase-basico",
+    storageBucket: "curso-firebase-basico.appspot.com",
+    messagingSenderId: "820608390043"
+  };
+  firebase.initializeApp(config);
+  
